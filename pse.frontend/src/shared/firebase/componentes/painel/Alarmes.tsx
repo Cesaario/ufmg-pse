3940481e7a3127a779f74aa21b952789058651d7
Gustavo Cesário <gustavo.cesario@dtidigital.com.br>
2022-07-05 16:07:36 -0300

    grafico e alarmes

--- pse.frontend/src/shared/firebase/componentes/painel/Alarmes.tsx
+++ pse.frontend/src/shared/firebase/componentes/painel/Alarmes.tsx
@@ -1,7 +1,86 @@
-import { Typography } from "@mui/material"
+import {
+  SxProps,
+  Box,
+  useMediaQuery,
+  useTheme,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Paper,
+} from "@mui/material";
+import { getApp } from "firebase/app";
+import { getDatabase, ref } from "firebase/database";
+import { useObjectVal } from "react-firebase-hooks/database";
+
+const estiloPainel: SxProps = {
+  display: "flex",
+  alignItems: "start",
+  width: "40vw",
+  height: "65vh",
+};
+
+const estiloPainelMobile: SxProps = {
+  display: "flex",
+  alignItems: "center",
+  width: "90vw",
+  height: "65vh",
+};
+
+const estiloContainerGrafico: SxProps = {
+  height: "50%",
+  width: "100%",
+};
+
+interface Alarme {
+  tipo: string;
+  dataHora: string;
+}
 
 const Alarmes = () => {
-    return <Typography>Alarmes aqui</Typography>
-}
+  const theme = useTheme();
+  const telaPequena = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const database = getDatabase(getApp());
+  const [dados] = useObjectVal<Alarme[]>(ref(database, "alarmes"));
+
+  return (
+    <Box sx={telaPequena ? estiloPainelMobile : estiloPainel}>
+      <Box sx={estiloContainerGrafico}>
+        <TableContainer component={Paper}>
+          <Table size="small" aria-label="a dense table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="right">Tipo do Alarme</TableCell>
+                <TableCell align="right">Descrição</TableCell>
+                <TableCell align="right">Horário</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {dados?.map((row) => (
+                <TableRow
+                  key={row.dataHora}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.tipo}
+                  </TableCell>
+                  <TableCell align="right">{`Alarme do tipo ${row.tipo}`}</TableCell>
+                  <TableCell align="right">
+                    {`${new Date(row.dataHora).toLocaleDateString()} ${new Date(
+                      row.dataHora
+                    ).toLocaleTimeString()}`}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Box>
+  );
+};
 
 export default Alarmes;
--- pse.frontend/src/shared/firebase/componentes/painel/Grafico.tsx
+++ pse.frontend/src/shared/firebase/componentes/painel/Grafico.tsx
@@ -1,7 +1,81 @@
-import { Typography } from "@mui/material"
+import { SxProps, Box, useMediaQuery, useTheme } from "@mui/material";
+import { getApp } from "firebase/app";
+import { getDatabase, ref } from "firebase/database";
+import { useObjectVal } from "react-firebase-hooks/database";
+import {
+  CartesianGrid,
+  LineChart,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Line,
+  ResponsiveContainer,
+} from "recharts";
+
+const estiloPainel: SxProps = {
+  display: "flex",
+  alignItems: "center",
+  width: "40vw",
+  height: "65vh",
+};
+
+const estiloPainelMobile: SxProps = {
+  display: "flex",
+  alignItems: "center",
+  width: "90vw",
+  height: "65vh",
+};
+
+const estiloContainerGrafico: SxProps = {
+  height: "50%",
+  width: "100%",
+  marginRight: "32px",
+};
+
+interface DadoHistorico {
+  dataHora: string;
+  valor: number;
+}
 
 const Grafico = () => {
-    return <Typography>Grafico aqui</Typography>
-}
+  const theme = useTheme();
+  const telaPequena = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const database = getDatabase(getApp());
+  const [dados] = useObjectVal<DadoHistorico[]>(
+    ref(database, "historicoTanque")
+  );
+
+  const formatarStringParaHorario = (dataString: string) => {
+    const data = new Date(dataString);
+    return `${data.getHours()}:${data.getMinutes()}`;
+  };
+
+  return (
+    <Box sx={telaPequena ? estiloPainelMobile : estiloPainel}>
+      <Box sx={estiloContainerGrafico}>
+        <ResponsiveContainer>
+          <LineChart
+            width={730}
+            height={250}
+            data={dados || []}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis
+              dataKey="dataHora"
+              tickFormatter={formatarStringParaHorario}
+            />
+            <YAxis domain={[0, 100]} />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="valor" stroke="#388e3c" />
+          </LineChart>
+        </ResponsiveContainer>
+      </Box>
+    </Box>
+  );
+};
 
 export default Grafico;
